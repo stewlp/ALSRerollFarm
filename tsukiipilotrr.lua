ef977c0a562a73b8935bf9b1ac03d86cb1cbcab5
Tsukii Sælge <mjarence0531@gmail.com>
2024-11-27 23:40:13 +0800

    Update tsukiipilotrr.lua

--- tsukiipilotrr.lua
+++ tsukiipilotrr.lua
@@ -97,96 +97,96 @@ local function savePlayerStats(playerName, playerLevel, emeralds, gold, rerolls,
    savePlayerStats(playerName, playerLevel, emeralds, gold, rerolls, als_jewels, rewards)
 end
 local function JoinINFCastle()
-   while PlaceId == 12886143095 do
-   wait(5)
-   game:GetService("RunService"):Set3dRenderingEnabled(False)
-   local player = game.Players.LocalPlayer
-   local character = player.Character or player.CharacterAdded:Wait()
-   local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-
-   local position = Vector3.new(28.3693733, 2.72691631, -40.4032669)
-   local lookVector = Vector3.new(-0.766061664, 0, 0.642767608)
-   local upVector = Vector3.new(0, 1, 0)
-   local rightVector = lookVector:Cross(upVector)
-   
-   local InfinityCastleManager = CFrame.fromMatrix(position, rightVector, upVector)
-   
-   humanoidRootPart.CFrame = InfinityCastleManager
-
-   local args = {
-       [1] = "Play",
-       [2] = 5,
-       [3] = "True"
-   }
-
-   game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("InfiniteCastleManager"):FireServer(unpack(args))
-   end
+    while game.PlaceId == 12886143095 do
+        wait(5)
+        game:GetService("RunService"):Set3dRenderingEnabled(false)
+        local player = game.Players.LocalPlayer
+        local character = player.Character or player.CharacterAdded:Wait()
+        local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+        
+        local position = Vector3.new(28.3693733, 2.72691631, -40.4032669)
+        local lookVector = Vector3.new(-0.766061664, 0, 0.642767608)
+        local upVector = Vector3.new(0, 1, 0)
+        local rightVector = lookVector:Cross(upVector)
+        
+        local InfinityCastleManager = CFrame.fromMatrix(position, rightVector, upVector)
+        
+        humanoidRootPart.CFrame = InfinityCastleManager
+    
+        local args = {
+            [1] = "Play",
+            [2] = 5,
+            [3] = "True"
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("InfiniteCastleManager"):FireServer(unpack(args))
+    end
 end
 local function UpdateInGameStats()
-   local player = game.Players.LocalPlayer
-   local playerName = player.DisplayName
-   local playerStats = loadPlayerStats(playerName)
+    local player = game.Players.LocalPlayer
+    local playerName = player.DisplayName
+    local playerStats = loadPlayerStats(playerName)
    local rerolls = playerStats.rerolls + 2
    local rewards = "<:rerolls:1305541972215201812> +2 "
    savePlayerStats(playerName, playerStats.playerLevel, playerStats.emeralds, playerStats.gold, rerolls, playerStats.als_jewels, rewards)
-   WebhookUpdate(playerName, playerStats.playerLevel, playerStats.emeralds, playerStats.gold, rerolls, playerStats.als_jewels, rewards)
+    WebhookUpdate(playerName, playerStats.playerLevel, playerStats.emeralds, playerStats.gold, rerolls, playerStats.als_jewels, rewards)
 end
 local function SpawnUnit1()
-   if not game:IsLoaded() then game.Loaded:Wait() end
-   -- VoteSpeed
-   local args = {
-       [1] = true
-   }
-   
-   game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("VoteChangeTimeScale"):FireServer(unpack(args))
-   -- Spawn
-   local unit = game:GetService("Players")[game.Players.LocalPlayer.Name].Slots.Slot1
-   local unitname = unit.Value
-   local Players = game:GetService("Players")
-   local player = Players.LocalPlayer
-   local args = {
-       [1] = unitname,
-       [2] = CFrame.new(-135.04019165039062, 197.93942260742188, 10.85269546508789, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-   }
-   
-   game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("PlaceTower"):FireServer(unpack(args))
-   -- CPU + GPU SAVER
-   game:GetService("RunService"):Set3dRenderingEnabled(false)
-   local character = player.Character or player.CharacterAdded:Wait()
-   local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-   local part = Instance.new("Part")
-   
-   part.Size = Vector3.new(4, 1, 4) 
-   part.Anchored = true 
-   part.Position = humanoidRootPart.Position - Vector3.new(0, humanoidRootPart.Size.Y / 2 + part.Size.Y / 2, 0)
-   part.Parent = game.Workspace
-   game.Workspace.PlaceOn:Destroy()
-   game.Workspace.Enemies:Destroy()
-   local function GameEnded()
-       local IsGameEnded = player.PlayerGui:FindFirstChild("EndGameUI")
-       return IsGameEnded ~= nil
-   end
-   repeat wait(5)
-       local tower = workspace:FindFirstChild("Towers"):FindFirstChild(unitname)
-       if tower then
-           local upgradeArgs = {
-               [1] = tower
-           }
-           game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Upgrade"):InvokeServer(unpack(upgradeArgs))
-       end
-   until GameEnded()
-   
-   if GameEnded() then
-       UpdateInGameStats()
-       game:GetService("TeleportService"):Teleport(12886143095)
-   end
+    if not game:IsLoaded() then game.Loaded:Wait() end
+    -- VoteSpeed
+    local args = {
+        [1] = true
+    }
+    
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("VoteChangeTimeScale"):FireServer(unpack(args))
+    -- Spawn
+    local unit = game:GetService("Players")[game.Players.LocalPlayer.Name].Slots.Slot1
+    local unitname = unit.Value
+    local Players = game:GetService("Players")
+    local player = Players.LocalPlayer
+    local args = {
+        [1] = unitname,
+        [2] = CFrame.new(-135.04019165039062, 197.93942260742188, 10.85269546508789, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+    }
+    
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("PlaceTower"):FireServer(unpack(args))
+    -- CPU + GPU SAVER
+    game:GetService("RunService"):Set3dRenderingEnabled(false)
+    local character = player.Character or player.CharacterAdded:Wait()
+    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+    local part = Instance.new("Part")
+    
+    part.Size = Vector3.new(4, 1, 4) 
+    part.Anchored = true 
+    part.Position = humanoidRootPart.Position - Vector3.new(0, humanoidRootPart.Size.Y / 2 + part.Size.Y / 2, 0)
+    part.Parent = game.Workspace
+    game.Workspace.PlaceOn:Destroy()
+    game.Workspace.Enemies:Destroy()
+    local function GameEnded()
+        local IsGameEnded = player.PlayerGui:FindFirstChild("EndGameUI")
+        return IsGameEnded ~= nil
+    end
+    repeat wait(5)
+        local tower = workspace:FindFirstChild("Towers"):FindFirstChild(unitname)
+        if tower then
+            local upgradeArgs = {
+                [1] = tower
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Upgrade"):InvokeServer(unpack(upgradeArgs))
+        end
+    until GameEnded()
+    
+    if GameEnded() then
+        UpdateInGameStats()
+        game:GetService("TeleportService"):Teleport(12886143095)
+    end
 end
 if game.PlaceId == LobbyPlaceId then
-   UpdatePlayerStats()
-   JoinINFCastle()
-   wait(120)
-   game:GetService("TeleportService"):Teleport(12886143095)
+    UpdatePlayerStats()
+    JoinINFCastle()
+    wait(120)
+    game:GetService("TeleportService"):Teleport(12886143095)
 else 
-   wait(3)
-   SpawnUnit1()
+    wait(3)
+    SpawnUnit1()
 end
